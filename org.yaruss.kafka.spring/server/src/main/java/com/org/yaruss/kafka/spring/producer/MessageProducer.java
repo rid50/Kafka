f4ea2f4aacc1917c5cfedabd9278ac5fdc405453
rid50 <rdavidenko@gmail.com>
2026-02-12 22:05:52 +0300

    org.yaruss.kafka.spring

---
+++ org.yaruss.kafka.spring/server/src/main/java/com/org/yaruss/kafka/spring/producer/MessageProducer.java
@@ -0,0 +1,50 @@
+package com.org.yaruss.kafka.spring.producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import jakarta.annotation.PostConstruct;
+
+//import com.roytuts.spring.apache.kafka.streams.websocket.stomp.server.service.GreetingService;
+import com.org.yaruss.kafka.spring.service.GreetingService;
+
+@Component
+@EnableScheduling
+public class MessageProducer {
+    // ANSI escape code constants
+    public static final String RESET = "\u001B[0m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String RED = "\u001B[31m";	
+	
+	@Value("${kafka.input.topic}")
+	private String kafkaInputTopic;
+
+	@Autowired
+	private GreetingService greetingService;
+
+	// @Autowired
+	// private KafkaTemplate<String, String> kafkaTemplate;
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public MessageProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+	
+	//@Bean
+	@Scheduled(fixedRate = 1000)
+	//@PostConstruct
+	//public void produce(KafkaTemplate<String, String> kafkaTemplate) {	
+	public void produce() {
+		System.out.println(RED + "****************************************************************" + RESET);		
+		String msg = greetingService.greet();
+		//System.out.println(RED + "Greeting Message :: " + msg + RESET);
+
+		this.kafkaTemplate.send(kafkaInputTopic, msg);
+	}
+
+}
