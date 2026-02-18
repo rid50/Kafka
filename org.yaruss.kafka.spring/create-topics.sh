0a03f8e2be9fd6ad086d066e86ab7fa2317f6c90
rid50 <rdavidenko@gmail.com>
2026-02-19 01:55:46 +0300

    docker

--- org.yaruss.kafka.spring/create-topics.sh
+++ org.yaruss.kafka.spring/create-topics.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-/opt/bitnami/kafka/bin/kafka-topics.sh --create --topic yaruss-input --bootstrap-server kafka:9092
-echo "topic yaruss-input was create"
+echo "Start: Sleep 15 seconds"
+sleep 30;
+wait;
+echo "Begin creating topics"
+docker exec Kafka1 kafka-topics --create --if-not-exists --topic yaruss-input --bootstrap-server kafka:9092 --partitions 1 --replication-factor 3
+/opt/bitnami/kafka/bin/kafka-topics.sh --create --topic yaruss-input --bootstrap-server kafka:9092 --partitions 1 --replication-factor 3
+echo "Done creating topics"
